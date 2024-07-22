7a0d5a81937b59ec16f50f16db89f86da23f1bb5
Cheryl Pappenheimer <cheryl.theroux@gmail.com>
2024-07-22 17:21:42 -0400

    No false sharing exercise.

--- OMP_Exercises/pi_omp.c
+++ OMP_Exercises/pi_omp.c
@@ -2,9 +2,9 @@
 
 This program will numerically compute the integral of
 
-                  4/(1+x*x) 
-				  
-from 0 to 1.  The value of this integral is pi -- which 
+				  4/(1+x*x)
+
+from 0 to 1.  The value of this integral is pi -- which
 is great since it gives us an easy way to check the answer.
 
 The is the original sequential program.  It uses the timer
@@ -18,82 +18,47 @@ History: Written by Tim Mattson, 11/99.
 #include <algorithm>
 #include <iostream>
 
-
-int main ()
+int main()
 {
-	  static const long num_steps = 100000000;
-	  //static const long num_steps = 100;
-	  double step;
+	static const long NUM_STEPS = 100000000;
 
-	  double start_time, run_time;
-	  //const unsigned int NUM_THREAD_REQ = 4;
-	  const unsigned int NUM_THREAD_REQ = std::max(atoi(std::getenv("OMP_NUM_THREADS")), 1);
-	  std::cout << "Num threads: " << NUM_THREAD_REQ << std::endl;
+	double start_time = omp_get_wtime();
 
-	  step = 1.0/(double) num_steps;
+	double step = 1.0 / (double)NUM_STEPS;
 
-	  start_time = omp_get_wtime();
-
-	  double sum[NUM_THREAD_REQ];
-	  for (int i=0; i<NUM_THREAD_REQ; i++)
-	  {
-		sum[i] = 0.0;
-	  }
-
-	  //omp_set_num_threads(NUM_THREAD_REQ);
-	  #pragma omp parallel
-	  {
+	double tot_sum = 0.0;
+	#pragma omp parallel
+	{
 		int ID = omp_get_thread_num();
+		int num_threads = omp_get_num_threads();
 
-		const unsigned int NUM_THREADS = omp_get_num_threads();
-		const unsigned int NUM_STEPS_PER_THREAD = num_steps / NUM_THREADS;
-		const unsigned int NUM_STEPS_LEFTOVER = num_steps % NUM_THREADS;
-		// printf("Num threads: %d\n", NUM_THREADS);
-		// printf("Num steps per thread: %d\n", NUM_STEPS_PER_THREAD);
-		// printf("Num steps leftover: %d\n", NUM_STEPS_LEFTOVER);
 		if (ID == 0)
 		{
-			printf("OMP using %d threads", NUM_THREADS);
+			printf("OMP using %d threads", num_threads);
 		}
 
-		int start = NUM_STEPS_PER_THREAD * ID + 1;
-		int end = (ID+1) * NUM_STEPS_PER_THREAD;
-		if (ID == NUM_THREADS-1)
-		{
-			end = num_steps;
-		}
-
-		//printf("\n Thread %d starts loop at %d, ends at %d\n ", ID, start, end);
-		sum[ID] == 0.0;
+		int num_steps_per_thread = NUM_STEPS / num_threads;
+		int start = num_steps_per_thread*ID + 1;
+		int end = (ID == num_threads-1) ? NUM_STEPS : (ID+1) * num_steps_per_thread;
+	
+		// printf("\n Thread %d starts loop at %d, ends at %d\n ", ID, start, end);
+		double thread_sum = 0.0;
 		for (int i=start; i<=end; i++)
 		{
-			double x = (i-0.5)*step;
-			sum[ID] = sum[ID] + 4.0/(1.0+x*x);
-			// if (ID == 0)
-			// {
-			// 	printf("i: %d\n", i);
-			// 	if (i>end && ID == 0)
-			// 	{
-			// 		printf("BREAK: i=%d, end=%i\n", i, end);
-			// 	}
-			// }
+			double x = (i - 0.5) * step;
+			thread_sum += 4.0 / (1.0 + x * x);
 		}
-	  }
 
-	  double pi = 0.0;
-	  for (int i = 0; i < NUM_THREAD_REQ; i++)
-	  {
-		//printf("Sum[%d] = %f\n", i, sum[i]);
-		pi += step * sum[i];
-	  }
+		#pragma omp critical
+		{
+			tot_sum += thread_sum;
+		}
+	} // end omp parallel
 
-	  run_time = omp_get_wtime() - start_time;
+	double pi = step * tot_sum;
 
-	  //Expect: pi with 100000000 steps is 3.141593 in 0.371373 seconds
-	  printf("\n pi with %ld steps is %lf in %lf seconds\n ",num_steps,pi,run_time);
-}	  
+	double run_time = omp_get_wtime() - start_time;
 
-
-
-
-
+	// Single thread result: pi with 100000000 steps is 3.141593 in 0.371373 seconds
+	printf("\nPi with %ld steps is %lf in %lf seconds\n ", NUM_STEPS, pi, run_time);
+}
--- pi_omp.sh
+++ pi_omp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export OMP_NUM_THREADS=6
+export OMP_NUM_THREADS=12
 ./bin/pi_omp
